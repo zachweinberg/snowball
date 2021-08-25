47970e1a801a31af740f1afd230d8c54d70e1add
Zach Weinberg <zweinberg3@gmail.com>
2021-08-24 22:15:01 -0400

    feat: new add asset routes

--- src/controllers/positions.ts
+++ src/controllers/positions.ts
@@ -1,9 +1,32 @@
-import { AddStockRequest, AssetType, Portfolio, StockPosition } from '@zachweinberg/wealth-schema';
+import {
+  AddCashRequest,
+  AddCryptoRequest,
+  AddRealEstateRequest,
+  AddStockRequest,
+  AssetType,
+  CashPosition,
+  CryptoPosition,
+  Portfolio,
+  RealEstatePosition,
+  StockPosition,
+} from '@zachweinberg/wealth-schema';
 import { Router } from 'express';
 import { catchErrors, requireSignedIn } from '~/utils/api';
 import { createDocument, fetchDocument } from '~/utils/db';
 
 const positionsRouter = Router();
+
+const userOwnsPortfolio = async (req, res, portfolioID) => {
+  const userID = req.authContext!.uid;
+
+  const portfolio = await fetchDocument<Portfolio>('portfolios', portfolioID);
+
+  if (portfolio.userID !== userID) {
+    return false;
+  }
+
+  return true;
+};
 
 positionsRouter.post(
   '/stock',
@@ -12,11 +35,7 @@ positionsRouter.post(
     const { portfolioID, symbol, companyName, quantity, costBasis, note } =
       req.body as AddStockRequest;
 
-    const userID = req.authContext!.uid;
-
-    const portfolio = await fetchDocument<Portfolio>('portfolios', portfolioID);
-
-    if (portfolio.userID !== userID) {
+    if (!userOwnsPortfolio(req, res, portfolioID)) {
       return res.status(401).json({ status: 'error', error: 'Invalid.' });
     }
 
@@ -26,6 +45,90 @@ positionsRouter.post(
       costBasis,
       quantity,
       symbol: symbol.toUpperCase(),
+      createdAt: new Date(),
+      note: note ? note : '',
+    });
+
+    const response = {
+      status: 'ok',
+    };
+
+    res.status(200).json(response);
+  })
+);
+
+positionsRouter.post(
+  '/crypto',
+  requireSignedIn,
+  catchErrors(async (req, res) => {
+    const { portfolioID, symbol, coinName, quantity, costBasis, note } =
+      req.body as AddCryptoRequest;
+
+    if (!userOwnsPortfolio(req, res, portfolioID)) {
+      return res.status(401).json({ status: 'error', error: 'Invalid.' });
+    }
+
+    await createDocument<CryptoPosition>(`portfolios/${portfolioID}/positions`, {
+      assetType: AssetType.Crypto,
+      coinName,
+      costBasis,
+      quantity,
+      symbol: symbol.toUpperCase(),
+      createdAt: new Date(),
+      note: note ? note : '',
+    });
+
+    const response = {
+      status: 'ok',
+    };
+
+    res.status(200).json(response);
+  })
+);
+
+positionsRouter.post(
+  '/real-estate',
+  requireSignedIn,
+  catchErrors(async (req, res) => {
+    const { portfolioID, address, estimatedAppreciationRate, propertyType, propertyValue, note } =
+      req.body as AddRealEstateRequest;
+
+    if (!userOwnsPortfolio(req, res, portfolioID)) {
+      return res.status(401).json({ status: 'error', error: 'Invalid.' });
+    }
+
+    await createDocument<RealEstatePosition>(`portfolios/${portfolioID}/positions`, {
+      assetType: AssetType.RealEstate,
+      propertyType,
+      propertyValue,
+      estimatedAppreciationRate,
+      createdAt: new Date(),
+      note: note ? note : '',
+      address: address ? address : '',
+    });
+
+    const response = {
+      status: 'ok',
+    };
+
+    res.status(200).json(response);
+  })
+);
+
+positionsRouter.post(
+  '/cash',
+  requireSignedIn,
+  catchErrors(async (req, res) => {
+    const { portfolioID, amount, accountName, note } = req.body as AddCashRequest;
+
+    if (!userOwnsPortfolio(req, res, portfolioID)) {
+      return res.status(401).json({ status: 'error', error: 'Invalid.' });
+    }
+
+    await createDocument<CashPosition>(`portfolios/${portfolioID}/positions`, {
+      assetType: AssetType.Cash,
+      accountName,
+      amount,
       createdAt: new Date(),
       note: note ? note : '',
     });
