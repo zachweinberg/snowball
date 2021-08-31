67e03055ae07a01e081735bc4b8caf63b1c4a732
Zach Weinberg <zweinberg3@gmail.com>
2021-08-31 13:11:38 -0400

    feat: home page spinner

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import Spinner from '~/components/Spinner';
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  });
+
+  return (
+    <div className="flex items-center justify-center w-full mt-20">
+      <Spinner size={40} />
+    </div>
+  );
+};
+
+export default Home;
