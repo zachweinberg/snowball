402ea96476da466a8cd3e432a45cefb2dc63eba7
Zach Weinberg <zweinberg3@gmail.com>
2021-09-09 18:22:39 -0400

    start svg chart

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,75 +1,7 @@
 import { ChevronDownIcon } from '@heroicons/react/outline';
-import { ChartData, ChartOptions } from 'chart.js';
 import { NextPage } from 'next';
-import React, { useRef } from 'react';
-import { Line } from 'react-chartjs-2';
-
-const data: ChartData = {
-  labels: ['a', 'b', '', ''],
-  datasets: [
-    {
-      data: [1, 2, 3, 5, 7, 6, 7, 6, 8, 14, 12, 12, 15, 16, 15, 15, 16, 17, 18],
-      tension: 0.5,
-      pointHoverRadius: 10,
-    },
-  ],
-};
-
-const Chart: React.FunctionComponent = () => {
-  const balanceTextRef = useRef<HTMLParagraphElement>(null);
-
-  const options: ChartOptions = {
-    animation: {
-      duration: 0,
-    },
-    hover: { intersect: false, mode: 'nearest' },
-    color: 'red',
-    responsive: true,
-    elements: { point: { radius: 0 } },
-    maintainAspectRatio: true,
-    plugins: {
-      tooltip: {
-        enabled: false,
-      },
-      legend: { display: false },
-    },
-    layout: {
-      padding: {
-        top: 20,
-        bottom: 4,
-      },
-    },
-    onHover: (el) => {
-      if (!balanceTextRef || balanceTextRef.current === null) {
-        return;
-      }
-
-      // const balance = el.x;
-
-      // balanceTextRef.current.innerText = balance;
-    },
-    scales: {
-      xAxes: {
-        grid: { display: false },
-        display: true,
-        ticks: {
-          callback: (val) => {
-            return Number(val) % 4 === 0 ? val : '';
-          },
-          color: '#dadada',
-          font: { size: 12 },
-        },
-      },
-      yAxes: {
-        grid: { display: false },
-        display: false,
-        offset: true,
-      },
-    },
-  };
-
-  return <Line data={data} options={options} height={65} />;
-};
+import React from 'react';
+import LineChart from '~/components/LineChart';
 
 const ChartTest: NextPage = () => {
   return (
@@ -77,14 +9,30 @@ const ChartTest: NextPage = () => {
       className="max-w-3xl p-8 mx-auto mt-10 text-white rounded-3xl font-poppins"
       style={{ backgroundColor: '#141414' }}
     >
-      <div className="flex items-center justify-between w-full">
+      <div className="flex items-center justify-between w-full mb-8">
         <h1 className="text-2xl font-semibold">Growth</h1>
         <div className="flex items-center">
           <div className="mr-2 text-xl font-medium whitespace-nowrap">All-time</div>
           <ChevronDownIcon className="w-7 h-7" />
         </div>
       </div>
-      <Chart />
+
+      <LineChart
+        width={700}
+        height={300}
+        precision={2}
+        horizontalGuides={8}
+        data={[
+          { value: 10000, label: '9/2/2021' },
+          { value: 10400, label: '9/3/2021' },
+          { value: 10600, label: '9/4/2021' },
+          { value: 10500, label: '9/5/2021' },
+          { value: 10700, label: '9/6/2021' },
+          { value: 10600, label: '9/7/2021' },
+          { value: 11200, label: '9/8/2021' },
+          { value: 13200, label: '9/9/2021' },
+        ]}
+      />
     </div>
   );
 };
