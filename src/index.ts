02036f47aa7b4d091ed0a6827e5243d29c45a7a8
Zach Weinberg <zweinberg3@gmail.com>
2021-10-06 11:00:35 -0400

    change cost name

--- src/index.ts
+++ src/index.ts
@@ -58,14 +58,14 @@ export interface StockPosition extends Position {
   symbol: string;
   companyName: string;
   quantity: number;
-  costBasis: number;
+  costPerShare: number;
 }
 
 export interface CryptoPosition extends Position {
   symbol: string;
   coinName: string;
   quantity: number;
-  costBasis: number;
+  costPerCoin: number;
   logoURL?: string;
 }
 
@@ -142,7 +142,7 @@ export interface VerifyEmailResponse extends BaseResponse {
 export interface AddStockRequest {
   portfolioID: string;
   symbol: string;
-  costBasis: number;
+  costPerShare: number;
   companyName: string;
   quantity: number;
   note?: string;
@@ -151,7 +151,7 @@ export interface AddStockRequest {
 export interface AddCryptoRequest {
   portfolioID: string;
   symbol: string;
-  costBasis: number;
+  costPerCoin: number;
   coinName: string;
   quantity: number;
   note?: string;
