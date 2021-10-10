5587e9e0d580c368acab177a5a206ac813434617
zachweinberg <zachweinberg@users.noreply.github.com>
2021-10-10 23:02:47 +0000

    ci build type files [ci skip]

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -4,6 +4,8 @@ export interface User {
     investingExperienceLevel: InvestingExperienceLevel;
     name: string;
     createdAt: Date;
+    verified: boolean;
+    verificationCode?: string;
 }
 export declare enum InvestingExperienceLevel {
     LessThanOneYear = "Less than a year",
