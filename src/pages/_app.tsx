5f6f6bc42892e4f298cba590e4ddf172675172a1
Zach Weinberg <zweinberg3@gmail.com>
2021-10-19 12:42:16 -0400

    feat: add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import WaitForAuth from '~/components/auth/WaitForAuth';
 import { AuthProvider } from '~/context/AuthContext';
 import '../styles/globals.css';
 
+const HJID = 2659232;
+const HSJV = 6;
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    hotjar.initialize(HJID, HSJV);
+  }, []);
+
   return (
     <AuthProvider>
       <WaitForAuth>
