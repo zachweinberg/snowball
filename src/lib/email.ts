b5eba4671b5d69f3f7dab30039021955bb7f05f6
Zach Weinberg <zweinberg3@gmail.com>
2021-10-25 12:47:39 -0400

    feat: test MJML

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,3 +1,5 @@
+import mjml from 'mjml';
+import mustache from 'mustache';
 import * as postmark from 'postmark';
 
 const emailClient = new postmark.ServerClient(process.env.POSTMARK_API_KEY!);
@@ -20,11 +22,14 @@ export const sendEmail = async (
 };
 
 export const sendVerifyEmailEmail = async (toEmail: string, token: string, userID: string) => {
-  const html = `
-    <h1>Welcome to Obsidian Tracker.</h1>
-    <p>We can't wait to help you track your net worth.</p>
-    <a href="https://obsidiantracker.com/verify?t=${token}&u=${userID}">Click here to verify your email address</a>
-  `;
+  const renderedMJML = mustache.render(mjmlTemplate, { user: 'Zach' });
+  const html = mjml(renderedMJML).html;
+
+  // const html = `
+  //   <h1>Welcome to Obsidian Tracker.</h1>
+  //   <p>We can't wait to help you track your net worth.</p>
+  //   <a href="https://obsidiantracker.com/verify?t=${token}&u=${userID}">Click here to verify your email address</a>
+  // `;
 
   const text = `
   Welcome to Obsidian Tracker.\n
@@ -35,3 +40,17 @@ export const sendVerifyEmailEmail = async (toEmail: string, token: string, userI
 
   await sendEmail(toEmail, 'Please verify your email address', html, text, 'verify-email');
 };
+
+const mjmlTemplate = `
+<mjml>
+  <mj-body>
+    <mj-section>
+      <mj-column>
+        <mj-image width="100px" src="https://mjml.io/assets/img/logo-small.png"></mj-image>
+        <mj-divider border-color="#F45E43"></mj-divider>
+        <mj-text font-size="20px" color="#F45E43" font-family="helvetica">Hello {{user}}</mj-text>
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+`;
