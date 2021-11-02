6f418fb6d1319055c52547b7f7844078cb88625c
Zach Weinberg <zweinberg3@gmail.com>
2021-11-02 16:33:56 -0400

    new response

--- src/index.ts
+++ src/index.ts
@@ -141,6 +141,9 @@ export interface GetPortfoliosResponse extends BaseResponse {
   portfolios: PortfolioWithBalances[];
 }
 
+export interface GetPortfolioDailyBalancesResponse extends BaseResponse {
+  dailyBalances: DailyBalance[];
+}
 export interface GetPortfolioResponse extends BaseResponse {
   portfolio: PortfolioWithQuotes;
 }
