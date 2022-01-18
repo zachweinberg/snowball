ad8dded869da2e282ef26988087c54cd75197c10
Zach Weinberg <zweinberg3@gmail.com>
2022-01-17 19:59:04 -0500

    fix: fix history page

--- src/pages/portfolios/[portfolioID]/history.tsx
+++ src/pages/portfolios/[portfolioID]/history.tsx
@@ -25,12 +25,11 @@ const PortfolioLogsPage: NextPage = () => {
       setLogs(resp.logItems);
     } catch (err) {
       if (err.response.status === 404) {
-        router.push('/portfolios');
+        setError("Sorry, we couldn't find that portfolio.");
       } else {
         setError(
-          'Something went wrong while loading this page. Please contact support if this persists.'
+          'Something went wrong while loading this portfolio. Please contact support if this persists.'
         );
-        setLoading(false);
       }
     } finally {
       setLoading(false);
@@ -41,49 +40,51 @@ const PortfolioLogsPage: NextPage = () => {
     loadPortfolioLogs();
   }, []);
 
-  if (error) {
-    return (
-      <div className="max-w-md p-8 mx-auto mt-8 bg-white rounded-md">
-        <p className="mb-6 text-xl font-medium text-center text-purple2">{error}</p>
-        <Link href="/portfolios">
-          <Button type="button">Go Back</Button>
-        </Link>
-      </div>
-    );
-  }
+  const renderContent = () => {
+    if (error) {
+      return (
+        <div className="max-w-md p-8 mx-auto mt-8 bg-white rounded-md">
+          <p className="mb-6 text-xl font-medium text-center text-purple2">{error}</p>
+          <Link href="/portfolios">
+            <Button type="button">Go Back</Button>
+          </Link>
+        </div>
+      );
+    }
 
-  return (
-    <Layout title={`History - Obsidian Tracker`}>
-      {loading ? (
+    if (loading) {
+      return (
         <div className="flex items-center justify-center mt-32">
           <Spinner size={40} />
         </div>
-      ) : (
-        <>
-          <div className="flex items-center">
-            {auth.user && (
-              <Link href={`/portfolios/${router.query.portfolioID}`}>
-                <ArrowCircleLeftIcon className="w-6 h-6 mr-3 cursor-pointer hover:opacity-70" />
-              </Link>
-            )}
+      );
+    }
 
-            <h1 className="font-bold text-[1.75rem] mr-4">Portfolio History</h1>
-          </div>
+    return (
+      <>
+        <div className="flex items-center">
+          <Link href={`/portfolios/${router.query.portfolioID}`}>
+            <ArrowCircleLeftIcon className="w-6 h-6 mr-3 cursor-pointer hover:opacity-70" />
+          </Link>
 
-          <ul className="mt-8 space-y-2">
-            {logs.map((log) => (
-              <li className="flex items-center justify-between p-3 bg-white border rounded-md border-bordergray">
-                <p>{log.description}</p>
-                <p className="text-sm text-darkgray">
-                  {DateTime.fromMillis(log.createdAt).toFormat('LLL d yyyy, t')}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-    </Layout>
-  );
+          <h1 className="font-bold text-[1.75rem] mr-4">Portfolio History</h1>
+        </div>
+
+        <ul className="mt-8 space-y-2">
+          {logs.map((log) => (
+            <li className="flex items-center justify-between p-3 bg-white border rounded-md border-bordergray">
+              <p>{log.description}</p>
+              <p className="text-sm text-darkgray">
+                {DateTime.fromMillis(log.createdAt).toFormat('LLL d yyyy, t')}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  return <Layout title={`History - Obsidian Tracker`}>{renderContent()}</Layout>;
 };
 
 export default PortfolioLogsPage;
