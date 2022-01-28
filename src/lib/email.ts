50e0a7d353d9b5dbc81e06c1dd964b348e7d9a14
Zach Weinberg <zweinberg3@gmail.com>
2022-01-27 22:02:25 -0500

    fix: fix template id

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -24,7 +24,7 @@ const Emails = {
     messageStreamID: 'portfolio-summary',
   },
   portfolioReminder: {
-    templateAlias: '',
+    templateAlias: 'asset-alert-1',
     messageStreamID: 'portfolio-reminder',
   },
 };
