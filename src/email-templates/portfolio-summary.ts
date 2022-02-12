77f85cb7398504d52cc2804dffe79424d6e485cf
Zach Weinberg <zweinberg3@gmail.com>
2022-02-12 12:45:14 -0500

    chore: test margin

--- src/email-templates/portfolio-summary.ts
+++ src/email-templates/portfolio-summary.ts
@@ -2,29 +2,29 @@ import layout from './layout';
 
 const html = layout(`
       <mj-column>
-        <mj-text font-size="16px" line-height="1.4">
+        <mj-text font-size="15px" line-height="1.4">
          Hey {{fullName}}, here is your {{period}} Portfolio Summary for your portfolio <a href="https://obsidiantracker.com/portfolios/{{portfolioID}}">{{portfolioName}}</a>:
         </mj-text>
 
         <mj-divider border-color="#ccc"></mj-divider>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> As of {{dateStr}}: </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> As of {{dateStr}}: </mj-text>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> Cash: {{cashValue}} </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> Cash: {{cashValue}} </mj-text>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> Stocks: {{stocksValue}} </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> Stocks: {{stocksValue}} </mj-text>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> Crypto: {{cryptoValue}} </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> Crypto: {{cryptoValue}} </mj-text>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> Real Estate: {{realEstateValue}} </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> Real Estate: {{realEstateValue}} </mj-text>
 
-        <mj-text font-weight="500" font-size="16px" align="center"> Custom Assets: {{customsValue}} </mj-text>
+        <mj-text font-weight="500" font-size="15px" align="center"> Custom Assets: {{customsValue}} </mj-text>
 
-        <mj-text font-weight="600" font-size="16px" align="center"> Total: {{totalValue}} </mj-text>
+        <mj-text font-weight="600" font-size="15px" align="center"> Total: {{totalValue}} </mj-text>
 
         <mj-divider border-color="#ccc"></mj-divider>
 
-        <mj-text align="center">You can disable or edit these emails in your <a href="https://obsidiantracker.com/portfolios/{{portfolioID}}/settings">portfolio settings</a>.</mj-text>
+        <mj-text align="center" font-size="13px">You can disable or edit these emails in your <a href="https://obsidiantracker.com/portfolios/{{portfolioID}}/settings">portfolio settings</a>.</mj-text>
       </mj-column>`);
 
 const text = `Hi {{fullName}}, this is your {{period}} Portfolio Summary for your portfolio {{portfolioName}}.
