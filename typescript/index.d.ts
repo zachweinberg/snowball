f5a28b37b70e985ae74c9b37b3547e819b038b56
zachweinberg <zachweinberg@users.noreply.github.com>
2022-02-15 14:27:17 +0000

    ci build type files [ci skip]

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -96,9 +96,10 @@ export interface RealEstatePosition extends Position {
     mortgage: Mortgage | null;
 }
 export interface Mortgage {
-    endDate: Date;
-    monthlyPayment: number;
-    currentBalance: number;
+    startDate: Date;
+    term: 5 | 7 | 10 | 15 | 20 | 30;
+    rate: number;
+    startingBalance: number;
 }
 export interface CashPosition extends Position {
     amount: number;
